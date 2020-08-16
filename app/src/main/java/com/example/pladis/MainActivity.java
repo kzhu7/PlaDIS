dd14f70d80292929cd60852d25ea5f40b4f0b5b1
Kevin Zhu <kzgiant09@gmail.com>
2020-08-15 18:02:02 -0700

    First commit

--- app/src/main/java/com/example/pladis/MainActivity.java
+++ app/src/main/java/com/example/pladis/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.pladis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCapture;
+    private TextureView textureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.camera_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button pushed");
+            }
+        });
+    }
+
+    private void createCameraPreview() {
+
     }
 }
